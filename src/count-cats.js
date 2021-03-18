8d1650c4feaafe7decffa8624b4ebb42b011e3d0
Dmitry Likhachev <valomon@yandex.ru>
2021-03-18 11:35:03 +0500

    feat: add solve of cats-cout task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function countCats(matrix) {
+  return matrix.reduce((accumulator, currentValue)=>{
+  
+    /* return accumulator += currentValue.reduce((acc,cur)=>{
+        if(cur === '^^'){
+          return acc+=1;
+        }
+     }, 0); */
+    let count = 0;
+    for(let i =0; i<currentValue.length; i++){
+      if(currentValue[i]==='^^'){
+     count++
+     }
+    }
+    return accumulator+=count;
+ }, 0);
+ }
