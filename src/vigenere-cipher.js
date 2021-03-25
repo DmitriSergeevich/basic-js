acc781e4ee0d4a48cd3027928780785d0d405ae4
Dmitry Likhachev <valomon@yandex.ru>
2021-03-25 13:15:43 +0500

    feat: add vigenere-cipher task solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,55 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode=true){
+    this.mode=mode;
   }
+  encrypt(str, key){
+    const alfabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+      let codedletters = [];
+      if(str=== undefined || key=== undefined){
+      throw new Error('Хуле бля');
+      }
+      let uppedStr = str.toUpperCase();
+      let uppedKey = key.toUpperCase();
+      let j =0;
+      for(let i=0; i<uppedStr.length; i++){
+        if(uppedStr[i].charCodeAt()>64 && uppedStr[i].charCodeAt()<91){
+          codedletters.push(findeLetter(uppedStr[i]))
+          j===uppedKey.length-1 ? j=0 : j+=1
+        }else{
+          codedletters.push(uppedStr[i])
+        }
+  }
+  function findeLetter(letter){
+    return  alfabet[((letter.charCodeAt()-65)+(uppedKey[j].charCodeAt()-65))%26]
+  }
+  return this.mode ? codedletters.join('').toUpperCase() : codedletters.reverse().join('').toUpperCase();
+  }  
+    decrypt(str, key){
+      const alfabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+        let codedletters = [];
+        if(str=== undefined || key=== undefined){
+        throw new Error('Хуле бля');
+        }
+        let uppedStr = str.toUpperCase();
+        let uppedKey = key.toUpperCase();
+        let j =0;
+        for(let i=0; i<uppedStr.length; i++){
+          if(uppedStr[i].charCodeAt()>64 && uppedStr[i].charCodeAt()<91){
+            codedletters.push(findeLetter(uppedStr[i]))
+            j===uppedKey.length-1 ? j=0 : j+=1
+          }else{
+            codedletters.push(uppedStr[i])
+          }
+    }
+    function findeLetter(letter){
+      return  alfabet[(((letter.charCodeAt()-64)+26)-(uppedKey[j].charCodeAt()-64))%26]
+    }
+    
+    return this.mode ? codedletters.join('').toUpperCase() : codedletters.reverse().join('').toUpperCase();
+    
+    }
 }
 
 module.exports = VigenereCipheringMachine;
